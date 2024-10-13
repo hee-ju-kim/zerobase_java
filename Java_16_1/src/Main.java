79f16b320e1b510a3ee529c9de9b26b82ebfcb23
heeju <bbbb412@naver.com>
2024-10-13 15:47:21 +0900

    241013

---
+++ Java_16_1/src/Main.java
@@ -0,0 +1,31 @@
+// Java 프로그래밍 - 람다식
+
+interface ComputeTool {
+    public abstract int compute(int x, int y);
+
+    public abstract int compute2(int x, int y);
+}
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        ComputeTool cTool1 = new ComputeTool() {
+            @Override
+            public int compute(int x, int y) {
+                return x + y;
+            }
+            @Override
+            public int compute2(int x, int y) {
+                return x - y;
+            }
+        };
+
+        System.out.println(cTool1.compute(1,2));
+        // 람다식
+
+//        ComputeTool cTool2 = (x, y) -> { return x + y;}; // 구현해야될 함수가 두개이상이면 람다식으로는 구현 불가능
+//        System.out.println(cTool2.compute(1,2));
+    }
+}
