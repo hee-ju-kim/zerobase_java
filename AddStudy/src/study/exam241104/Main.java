b8fba70d36da5901ac40de7f09a2d909b2117d19
heeju <bbbb412@naver.com>
2024-11-04 22:59:25 +0900

    241104

---
+++ AddStudy/src/study/exam241104/Main.java
@@ -0,0 +1,24 @@
+package study.exam241104;
+
+/**
+ * 문서 주석 사용법
+ * */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int age;
+//        int 0age;
+//        int user age;
+        int $age;
+        int _age;
+        int 나이;
+        int age0;
+
+        // 한줄주석
+
+        /* 블록
+        주석 */
+
+    }
+}
